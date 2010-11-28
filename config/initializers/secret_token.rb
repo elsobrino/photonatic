e1f0589a97e3b1b10f36498ee9f82ba64e24a56a
Jan <janno@elsobrino.org>
2010-11-28 14:58:00 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photonatic::Application.config.secret_token = 'c844920dab033cd5252aab6c44ec7dd02d0ad204d5778824997ec0864a2c4039954d24278f77f5646c80b62d7e910f6ca032ab1f871b08e08ae898790fac15da'
