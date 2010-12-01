138e0ff1840053e7bbffca6aeac76f620b8587a6
Jan <janno@elsobrino.org>
2010-12-01 23:04:26 +0100

    user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :forename, :lastname, :email, :password, :password_confirmation, :username
@@ -21,6 +22,41 @@ class User < ActiveRecord::Base
   validates :password, :confirmation => true,
                        :presence => true,
                        :length => { :within => 6..40 }
+                       
+  before_save :encrypt_password
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    if user.has_password?(submitted_password)
+      return user
+    else
+      return nil
+    end
+  end
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  private
+    
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
   
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,62 +19,62 @@ describe User do
   
   describe "Testing forename validation" do
   
-  it "should not require a forename" do
-    no_forename_user = User.new(@attr.merge(:forename => ""))
-    no_forename_user.should be_valid
-  end
-  
-  it "should require an alphabetical forename" do
-    non_alphabetical_forenames = %w[John! Mike% Nick123 K.]
-    non_alphabetical_forenames.each do |forename|
-      invalid_user = User.new(@attr.merge(:forename => forename))
-      invalid_user.should_not be_valid
+    it "should not require a forename" do
+      no_forename_user = User.new(@attr.merge(:forename => ""))
+      no_forename_user.should be_valid
     end
-  end
   
-  it "should pass with valid forename" do
-    valid_forenames = %w[Steven Hans Robert Theo]
-    valid_forenames.each do |forename|
-      valid_user = User.new(@attr.merge(:forename => forename))
-      valid_user.should be_valid
+    it "should require an alphabetical forename" do
+      non_alphabetical_forenames = %w[John! Mike% Nick123 K.]
+      non_alphabetical_forenames.each do |forename|
+        invalid_user = User.new(@attr.merge(:forename => forename))
+        invalid_user.should_not be_valid
+      end
     end
-  end
   
-  it "should reject too long forenames" do
-    long_forename = "a"*51
-    to_long_forename = User.new(@attr.merge(:forename => long_forename))
-    to_long_forename.should_not be_valid
-  end
+    it "should pass with valid forename" do
+      valid_forenames = %w[Steven Hans Robert Theo]
+      valid_forenames.each do |forename|
+        valid_user = User.new(@attr.merge(:forename => forename))
+        valid_user.should be_valid
+      end
+    end
+  
+    it "should reject too long forenames" do
+      long_forename = "a"*51
+      to_long_forename = User.new(@attr.merge(:forename => long_forename))
+      to_long_forename.should_not be_valid
+    end
   end
   
   describe "Testing lastname validation" do
   
-  it "should not require a lastname" do
-    no_lastname_user = User.new(@attr.merge(:lastname => ""))
-    no_lastname_user.should be_valid
-  end
-  
-  it "should require an alphabetical lastname" do
-    non_alphabetical_lastnames = %w[Meyer! Sch$ulz Schmidt1324 M.]
-    non_alphabetical_lastnames.each do |lastname|
-      invalid_user = User.new(@attr.merge(:lastname => lastname))
-      invalid_user.should_not be_valid
+    it "should not require a lastname" do
+      no_lastname_user = User.new(@attr.merge(:lastname => ""))
+      no_lastname_user.should be_valid
     end
-  end
   
-  it "should pass with valid lastnames" do
-    valid_lastnames = %w[Meyer Schulz Mueller Schmidt]
-    valid_lastnames.each do |lastname|
-      valid_user = User.new(@attr.merge(:lastname => lastname))
-      valid_user.should be_valid
+    it "should require an alphabetical lastname" do
+      non_alphabetical_lastnames = %w[Meyer! Sch$ulz Schmidt1324 M.]
+      non_alphabetical_lastnames.each do |lastname|
+        invalid_user = User.new(@attr.merge(:lastname => lastname))
+        invalid_user.should_not be_valid
+      end
     end
-  end
   
-  it "should reject too long lastnames" do
-    long_lastname = "a"*51
-    to_long_lastname = User.new(@attr.merge(:lastname => long_lastname))
-    to_long_lastname.should_not be_valid
-  end
+    it "should pass with valid lastnames" do
+      valid_lastnames = %w[Meyer Schulz Mueller Schmidt]
+      valid_lastnames.each do |lastname|
+        valid_user = User.new(@attr.merge(:lastname => lastname))
+        valid_user.should be_valid
+      end
+    end
+  
+    it "should reject too long lastnames" do
+      long_lastname = "a"*51
+      to_long_lastname = User.new(@attr.merge(:lastname => long_lastname))
+      to_long_lastname.should_not be_valid
+    end
   end
 
   describe "Testing username validation" do
@@ -97,62 +97,106 @@ describe User do
 
   describe "Testing email address validation" do
     
-  it "should require a email address" do
-    no_email_user = User.new(@attr.merge(:email => ""))
-    no_email_user.should_not be_valid
-  end
-  
-  it "should accept valid email addresses" do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-    addresses.each do |address|
-      valid_email_user = User.new(@attr.merge(:email => address))
-      valid_email_user.should be_valid
+    it "should require a email address" do
+      no_email_user = User.new(@attr.merge(:email => ""))
+      no_email_user.should_not be_valid
     end
-  end
-
-  it "should reject invalid email address" do
-    addresses = %w[user@foo,com user_at_foo.org example_user@foo.]
-    addresses.each do |address|
-      invalid_email_user = User.new(@attr.merge(:email => address))
-      invalid_email_user.should_not be_valid
-    end
-  end
   
-  it "should reject too long email address" do
-    long_email = "#{'a'*255}@ab.de"
-    to_long_email = User.new(@attr.merge(:email => long_email))
-    to_long_email.should_not be_valid
-  end
+    it "should accept valid email addresses" do
+      addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+      addresses.each do |address|
+        valid_email_user = User.new(@attr.merge(:email => address))
+        valid_email_user.should be_valid
+      end
+    end
 
-  it "should reject duplicate email addresses" do
-    User.create!(@attr)
-    User.new(@attr.merge(:username => "Hans")).should_not be_valid
-  end
+    it "should reject invalid email address" do
+      addresses = %w[user@foo,com user_at_foo.org example_user@foo.]
+      addresses.each do |address|
+        invalid_email_user = User.new(@attr.merge(:email => address))
+        invalid_email_user.should_not be_valid
+      end
+    end
+  
+    it "should reject too long email address" do
+      long_email = "#{'a'*255}@ab.de"
+      to_long_email = User.new(@attr.merge(:email => long_email))
+      to_long_email.should_not be_valid
+    end
+
+    it "should reject duplicate email addresses" do
+      User.create!(@attr)
+      User.new(@attr.merge(:username => "Hans")).should_not be_valid
+    end
   end
 
   describe "Testing password validation" do
   
-  it "should reject password/confirmation mismatch" do
-    User.new(@attr.merge(:password => 'foobaz')).
-              should_not be_valid
-  end
+    it "should reject password/confirmation mismatch" do
+      User.new(@attr.merge(:password => 'foobaz')).
+                should_not be_valid
+    end
   
-  it "should reject passwords shorter than 6 chars" do
-    User.new(@attr.merge(:password => 'abcde', 
-                         :password_confirmation => 'abcde')).
+    it "should reject passwords shorter than 6 chars" do
+      User.new(@attr.merge(:password => 'abcde', 
+                           :password_confirmation => 'abcde')).
               should_not be_valid
-   end
+    end
    
-   it "should reject passwords longer than 40 chars" do
-     long_password = 'a'*41
-     User.new(@attr.merge(:password => long_password,
-                          :password_confirmation => long_password)).
+    it "should reject passwords longer than 40 chars" do
+      long_password = 'a'*41
+      User.new(@attr.merge(:password => long_password,
+                           :password_confirmation => long_password)).
                should_not be_valid
     end
     
     it "should reject empty passwords" do
       User.new(@attr.merge(:password => "", :password_confirmation => "")).
                should_not be_valid
+    end
+  end
+  
+  describe "password encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    
+    it "should set the encrpyted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    describe "has_password? method" do
+      
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should be fale if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+    
+    describe "authenticate method" do
+      
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+      
+      it "should return nil for an email address with no user" do
+        noexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+        noexistent_user.should be_nil
+      end
+      
+      it "should return the user on email/password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
     end
   end
 end
